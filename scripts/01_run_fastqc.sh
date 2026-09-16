68eb0924181c84a39608fb9b30e5a669b0f1acf9
Emma Alexandra Jakobsson <ejakobsson@login8.hpc.binf.unibe.ch>
2026-09-16 13:03:58 +0200

    Fastqc script improvement.

--- scripts/01_run_fastqc.sh
+++ scripts/01_run_fastqc.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=40G
-#SBATCH --time=01:00:00
+#SBATCH --mem=8G
+#SBATCH --time=02:00:00
 #SBATCH --job-name=fastqc
 #SBATCH --mail-user=emma.jakobsson@students.unibe.ch
 #SBATCH --mail-type=end
-#SBATCH --output=/data/users/user/assembly_annotation_course/logs/output_fastqc_%j.o
-#SBATCH --error=/data/users/user/assembly_annotation_course/logs/error_fastqc_%j.e
-#SBATCH --partition=pibu_el8
+#SBATCH --output=/data/users/ejakobsson/assembly_annotation_course/logs/output_fastqc_%j.o
+#SBATCH --error=/data/users/ejakobsson/assembly_annotation_course/logs/error_fastqc_%j.e
+#SBATCH --partition=pshort_el8
 
 WORKDIR=/data/users/ejakobsson/assembly_annotation_course
 OUTDIR=$WORKDIR/read_QC
@@ -20,4 +20,5 @@ apptainer exec \
 /containers/apptainer/fastqc-0.12.1.sif \
 fastqc \
 --outdir $OUTDIR \
-$WORKDIR/Db-1/*.fastq.gz
+$WORKDIR/Db-1/*.fastq.gz \
+$WORKDIR/RNAseq_Sha/*.fastq.gz
